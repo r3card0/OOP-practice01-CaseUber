1baf3697ed80182bf49942376dbcfef21754b06c
r3card0 <ricardo.rdz@pm.me>
2022-01-12 11:10:08 -0600

    Imported class Car into main class, created object car

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,15 @@
+from car import Car
+
 if __name__=='__main__':
     print('Hola Mundo')
+    car = Car()
+    car.license     = 'KILL007'
+    car.driver      = 'James Bond'
+    car.passenger   = 1
+    print(vars(car))
+
+    car2 = Car()
+    car2.license    = 'BAT001'
+    car2.driver     = 'Batman'
+    car2.passenger  = 1
+    print(vars(car2))
