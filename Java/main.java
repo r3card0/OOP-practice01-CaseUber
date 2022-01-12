903b28764471835950e1e11f545a9e34afb87123
r3card0 <ricardo.rdz@pm.me>
2022-01-12 11:07:48 -0600

    Renamed the file Main and created th object car

--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,19 @@
-class main {
+class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.driver = "James Bond";
+        car.license = "KILL007";
+        car.passenger = 1;
+        //System.out.println("Car License " + car.license);
+        car.printDataCar();
+
+
+        Car car2 = new Car();
+        car2.driver = "Batman";
+        car2.license = "BAT001";
+        car2.passenger = 1;
+        //System.out.println("Car License " + car2.license);
+        car2.printDataCar();
     }
 }
