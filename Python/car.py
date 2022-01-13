388a0df8e35735c6273a11820a58b960db3fa2b7
r3card0 <ricardo.rdz@pm.me>
2022-01-13 15:26:36 -0600

    __init__() function created

--- Python/car.py
+++ Python/car.py
@@ -3,3 +3,7 @@ class Car:
     license     = str
     driver      = str
     passenger   = int
+
+    def __init__(self,license, driver):  # metodo constructor declarado
+        self.license  = license
+        self.driver   = driver
