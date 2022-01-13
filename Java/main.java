6791e5f61d30617f5bd98d2fde37751a73692dc7
r3card0 <ricardo.rdz@pm.me>
2022-01-13 17:22:31 -0600

    reduce the code for the method added in the class Car and class Account

--- Java/main.java
+++ Java/main.java
@@ -1,17 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.driver = "James Bond";
-        car.license = "KILL007";
+        Car car = new Car("KILL007", new Account("James Bond","GUN123"));
         car.passenger = 1;
         //System.out.println("Car License " + car.license);
         car.printDataCar();
 
 
-        Car car2 = new Car();
-        car2.driver = "Batman";
-        car2.license = "BAT001";
+        Car car2 = new Car("BAT001", new Account("Batman", "batId"));
         car2.passenger = 1;
         //System.out.println("Car License " + car2.license);
         car2.printDataCar();
