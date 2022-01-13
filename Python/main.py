ce28090784f502f2186b00f827fddf8464a05e58
r3card0 <ricardo.rdz@pm.me>
2022-01-13 15:29:49 -0600

    imported classes, Account and Car, and print some values

--- Python/main.py
+++ Python/main.py
@@ -1,15 +1,21 @@
 from car import Car
+from account import Account
 
-if __name__=='__main__':
+if __name__=="__main__":
     print('Hola Mundo')
-    car = Car()
-    car.license     = 'KILL007'
-    car.driver      = 'James Bond'
-    car.passenger   = 1
-    print(vars(car))
 
-    car2 = Car()
-    car2.license    = 'BAT001'
-    car2.driver     = 'Batman'
-    car2.passenger  = 1
-    print(vars(car2))
+    car = Car("BAT001", Account("Batman","batiId"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    # car = Car()     # método constructor
+    # car.license     = 'KILL007'
+    # car.driver      = 'James Bond'
+    # car.passenger   = 1
+    # print(vars(car))
+
+    # car2 = Car()
+    # car2.license    = 'BAT001'
+    # car2.driver     = 'Batman'
+    # car2.passenger  = 1
+    # print(vars(car2))
