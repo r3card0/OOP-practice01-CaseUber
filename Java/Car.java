61d90f4e580b6e9fd6cc449bf352620c33e09ceb
r3card0 <ricardo.rdz@pm.me>
2022-01-13 17:16:43 -0600

    function public  added in class Car

--- Java/Car.java
+++ Java/Car.java
@@ -1,10 +1,17 @@
 public class Car {
     Integer  id;
-    String license;
-    String driver;
+    String license; // placa del carro
+    Account driver; // herencia de la clase Account
     Integer passenger;
 
+    // método constructor
+    public Car(String license, Account driver){
+        this.license    = license;
+        this.driver     = driver;
+
+    }
+
     void printDataCar(){
-        System.out.println("License: " + license + " Driver: " + driver);
+        System.out.println("License: " + license + " Driver: " + driver.name);
     }
 }
