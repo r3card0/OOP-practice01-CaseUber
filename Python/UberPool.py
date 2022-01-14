5e4188542b15823c7f2b1b7da3d204baf795cf01
r3card0 <ricardo.rdz@pm.me>
2022-01-13 20:10:14 -0600

    Subclasses created in Python-> UberX, UberPool, UberVan and UberBlack

---
+++ Python/UberPool.py
@@ -0,0 +1,12 @@
+from io import BufferedRandom
+from car import Car
+
+class UberPool(Car):
+    brand   = str
+    model   = str
+
+
+    def __init__(self, license, driver, brand, model):
+        super().__init__(license, driver)
+        self.brand  = brand 
+        self.model  = model
