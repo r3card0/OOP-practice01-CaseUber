d80f8cbdbed95ce7e8644624ff92fc8285f0fb29
r3card0 <ricardo.rdz@pm.me>
2022-01-13 19:11:43 -0600

    Added public function __construct for Account class

--- PHP/Account.php
+++ PHP/Account.php
@@ -1,8 +1,13 @@
 <?php
 class Account {
-    public $id         = [];
-    public $name       = [];
-    public $document   = [];
-    public $email      = [];
-    public $password   = [];
+    public $id;
+    public $name;
+    public $document;
+    public $email;
+    public $password;
+
+    public function __construct($name,$document) {
+        $this->name         = $name;
+        $this-> document    = $document;
+    }
 }
