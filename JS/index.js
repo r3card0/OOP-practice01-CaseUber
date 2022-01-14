4cab3206b92cd9d668aaae7ea9b1ee682b24a10e
r3card0 <ricardo.rdz@pm.me>
2022-01-14 09:57:09 -0600

    Added values of uberX

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("KILL007", new Account("James Bond","GUN123"))
 car.passenger = 1;
 car.printDataCar();
+
+var uberX = new UberX("BAT001", new Account("Batman","batId"), "Batmobile", "BatTank")
+uberX.passenger = 4;
+uberX.printDataCar();
